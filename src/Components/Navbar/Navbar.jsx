ed6fc596c40cf37caed49b85ec5386c41a159955
borgesmj <borgesmj19@gmail.com>
2023-10-26 11:21:35 -0500

    fixing navbar bug

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = ({ categories, shoppingCart }) => {
   }, []);
 
   return (
-    <nav className="h-12 fixed w-full top-0 px-4 flex flex-row justify-between items-center bg-driftwood-500">
+    <nav className="z-[1] h-12 fixed w-full top-0 px-4 flex flex-row justify-between items-center bg-driftwood-500">
       <NavLink to="/" className="h-12 w-12 fixed left-8 z-[1]">
         <img
           src="https://borgesmj.github.io/sweet-bites/src/IMG/Logo.png"
