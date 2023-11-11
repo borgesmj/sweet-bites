9d41f7acc72dec6cff0e16c5f33b81a628e2e750
borgesmj <borgesmj19@gmail.com>
2023-11-10 21:07:13 -0500

    actualizando numero de tlf

--- src/Pages/Cart/CartPage.jsx
+++ src/Pages/Cart/CartPage.jsx
@@ -55,7 +55,7 @@ const CArtPage = ({ shoppingCart, eliminarProducto, setShoppingCart }) => {
       mensajeEnvio += `\nSub-Total:${subTotal}`;
 
       let whatsappLink =
-        "https://api.whatsapp.com/send?phone=573184255610&text=" +
+        "https://api.whatsapp.com/send?phone=573158142409&text=" +
         encodeURI(mensajeEnvio);
       window.open(whatsappLink, "_blank");
 
