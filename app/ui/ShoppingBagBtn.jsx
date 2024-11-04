b23edf5fe64df10f0ecde44b715f920823a8363b
borgesmj19 <borgesmj19@gmail.com>
2024-11-04 14:48:00 +0000

    shopping bag component

--- app/ui/ShoppingBagBtn.jsx
+++ app/ui/ShoppingBagBtn.jsx
@@ -1,9 +1,9 @@
-"use client";
+'use client'
 import { LuShoppingBag } from "react-icons/lu";
 export default function ShoppingBagBtn() {
     return (
         <div className="relative h-[50px] w-[50px] flex flex-col justify-center items-center  before:bg-red-500 before:absolute before:top-0 before:right-0 before:w-6 before:h-6 before:content-['4'] before:rounded-full before:text-white before:text-center before:font-bold before:text-md cursor-pointer transition-fast hover:text-[--button-bg-primary]">
-            <LuShoppingBag size="2em" />
+            <LuShoppingBag size="30" />
         </div>
     );
 }
