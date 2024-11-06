ecb8d92037ba2343195fa8e8a75ed26432db6f3f
borgesmj19 <borgesmj19@gmail.com>
2024-11-06 15:41:23 +0000

    added dynamic metadada

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Tienda",
+    description: "pgina de tienda",
+  };
+
+export default function Layout({ children }) {
+  return (
+    <>
+    {children}
+    </>
+  );
+}
