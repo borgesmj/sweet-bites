4d2cf53e5b39c309bf6fcc313ce516a6808282fc
borgesmj19 <borgesmj19@gmail.com>
2024-11-12 14:45:28 +0000

    added an empty cart message

--- app/ui/Formularios.jsx
+++ app/ui/Formularios.jsx
@@ -4,6 +4,7 @@ import ProductList from "./Checkout/ProductList";
 import DeliverOptions from "./Checkout/DeliverOptions";
 import TotalAmmount from "./Checkout/TotalAmmount";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 const Formularios = () => {
   const { cartList } = useCart();
   const [homeDelivery, setHomeDelivery] = useState(false);
@@ -17,7 +18,18 @@ const Formularios = () => {
   }, 0);
 
   if (cartList.length < 1) {
-    return <h1>no tienes productos</h1>;
+    return (
+      <div className="w-full h-dvh flex justify-center items-center gap-2">
+        <span>Disculpa, no tienes productos en tu carrito. Ve a la{" "}</span>
+        <Link
+          className="font-semibold text-blue-700 underline"
+          href={`/tienda`}
+        >
+          tienda
+        </Link>{" "}
+        <span>y selecciona los productos a comprar.</span>
+      </div>
+    );
   }
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -40,9 +52,13 @@ const Formularios = () => {
     >
       <div className="w-full p-4 md:w-3/4 lg:w-3/5 xl:w-1/2 gap-4 flex  flex-col items-center">
         <CouponForm />
-        <h2 className="text-lg font-semibold text-gray-800 mb-4 w-full text-center lg:text-2xl">Lista de productos:</h2>
+        <h2 className="text-lg font-semibold text-gray-800 mb-4 w-full text-center lg:text-2xl">
+          Lista de productos:
+        </h2>
         <ProductList cartList={cartList} />
-        <h2 className="text-lg font-semibold text-gray-800 mb-4 w-full text-center lg:text-2xl">Detalles de la entrega:</h2>
+        <h2 className="text-lg font-semibold text-gray-800 mb-4 w-full text-center lg:text-2xl">
+          Detalles de la entrega:
+        </h2>
         <DeliverOptions
           homeDelivery={homeDelivery}
           setHomeDelivery={setHomeDelivery}
@@ -56,10 +72,7 @@ const Formularios = () => {
           address={address}
         />
       </div>
-      <TotalAmmount
-        subTotal={subTotal}
-        discountPercent={discountPercent}
-      />
+      <TotalAmmount subTotal={subTotal} discountPercent={discountPercent} />
     </form>
   );
 };
