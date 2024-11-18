e0031f99bd22fe77050d9648b6b3ecbd178748cd
borgesmj19 <borgesmj19@gmail.com>
2024-11-18 12:56:48 +0000

    new filters style

--- app/ui/FilterDesktop.jsx
+++ app/ui/FilterDesktop.jsx
@@ -11,12 +11,12 @@ export default function FilterDesktop({ categories, currentCategory }) {
   };
   return (
     <div className="hidden lg:flex  w-full p-6  mx-auto flex-row items-center justify-center gap-2 text-xl text-[--text-100] font-semibold ">
-      <p className="text-right  w-1/2 mr-4">Filtrar por:</p>
-      <div className="categories flex flex-row flex-wrap gap-2 justify-start  w-1/2">
+      <p className="text-right  w-1/4 mr-4">Filtrar por:</p>
+      <div className="categories flex flex-row flex-wrap gap-2 justify-start  w-3/4">
         <span
             onClick={(e) => handleCategoryClick(e.target.dataset.value)}
           data-value=""
-          className={`capitalize p-1 border-solid border-2 border-[--accent-100] text-center w-fit transition-all hover:bg-[--accent-100] hover:text-[--bg-100] cursor-pointer ${currentCategory === "" ? 'bg-[--accent-100] text-[--bg-100]' : null}`}
+          className={`capitalize px-1 py-2 rounded-2xl text-md border-solid border-2 border-[--accent-100] text-center w-fit transition-all hover:bg-[--accent-100] hover:text-[--bg-100] cursor-pointer ${currentCategory === "" ? 'bg-[--accent-100] text-[--bg-100]' : null}`}
         >
           Todo
         </span>
@@ -29,7 +29,7 @@ export default function FilterDesktop({ categories, currentCategory }) {
             onClick={(e) => handleCategoryClick(e.target.dataset.value)}
             data-value={category}
             key={`category-${index}`}
-            className={`capitalize p-1 border-solid border-2 border-[--accent-100] text-center w-fit transition-all hover:bg-[--accent-100] hover:text-[--bg-100] cursor-pointer ${currentCategory === category ? 'bg-[--accent-100] text-[--bg-100]' : null}`}
+            className={`capitalize align-middle py-1 px-2 border-solid rounded-2xl border-2 border-[--accent-100] text-center text-md w-fit transition-all hover:bg-[--accent-100] hover:text-[--bg-100] cursor-pointer ${currentCategory === category ? 'bg-[--accent-100] text-[--bg-100]' : null}`}
           >
             {category}
           </span>
