00c8508999b7632e26de1958de8c131e6328ea73
borgesmj19 <borgesmj19@gmail.com>
2024-12-04 21:07:27 +0000

    fixign deploy errors pt2

--- app/neworder/page.jsx
+++ app/neworder/page.jsx
@@ -1,46 +1,54 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import DataServise from "@/lib/FirebaseService";
-export default function Page() {
-const params = useSearchParams();
-const getTimestamp = () => {
-  const timestamp = Date.now();
-  return timestamp;
-}
+import { useEffect, Suspense } from "react";
+import DataService from "@/lib/FirebaseService";
+
+function OrderSender() {
+  const params = useSearchParams();
+  const getTimestamp = () => Date.now();
+
   const sendOrder = async () => {
-    const newOrder = {}
+    const newOrder = {};
     for (const [key, value] of params.entries()) {
       newOrder[key] = value;
     }
     const products = localStorage.getItem("cartList");
-    newOrder.products = JSON.parse(products);
-    //* Status
-    //* 1: pendiente
-    //* 2: entregado
-    //* 3: cancelado
-    newOrder.status= 1;
-    newOrder.createdAt = getTimestamp()
-    // * aqui va la logica para enviar a firebase
-    const orderID = await DataServise.generateNewOrder(newOrder)
+    newOrder.products = JSON.parse(products || "[]");
+    newOrder.status = 1; // Pendiente
+    newOrder.createdAt = getTimestamp();
+
+    // Lógica para enviar a Firebase
+    const orderID = await DataService.generateNewOrder(newOrder);
     await localStorage.removeItem("cartList");
-    await localStorage.removeItem("coupon")
-    await localStorage.clear()
-    let messageText = ""
-    messageText += `Hola, he realizado un nuevo pedido. Con el ID: ${orderID}`
+    await localStorage.removeItem("coupon");
+    await localStorage.clear();
+
+    const messageText = `Hola, he realizado un nuevo pedido. Con el ID: ${orderID}`;
     const phoneNumber = process.env.NEXT_PUBLIC_PHONE_NUMBER;
     window.open(
-      `whatsapp://send?phone=${phoneNumber}&text=${messageText}`,
-      '_blank' // <- This is what makes it open in a new window.
+      `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(
+        messageText
+      )}`,
+      "_blank"
     );
-    window.location.href = "/"
+    window.location.href = "/";
   };
+
   useEffect(() => {
     sendOrder();
   }, []);
+
   return (
-    <div className="w-full h-dvh flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center">
       Enviando tu pedido, por favor no recargues la página.
     </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderSender />
+    </Suspense>
   );
 }
