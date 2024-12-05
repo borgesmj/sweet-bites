defca3bf3ff1d0357e77f2389e0b59f4274cf2ba
borgesmj19 <borgesmj19@gmail.com>
2024-12-05 19:13:18 +0000

    changed new order status

--- app/neworder/page.jsx
+++ app/neworder/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, Suspense } from "react";
+import { useEffect, Suspense, useState } from "react";
 import DataService from "@/lib/FirebaseService";
+import Link from "next/link";
 
 function OrderSender() {
   const params = useSearchParams();
   const getTimestamp = () => Date.now();
+  const [status, setStatus] = useState(2);
 
   const sendOrder = async () => {
     const newOrder = {};
@@ -25,13 +27,12 @@ function OrderSender() {
 
     const messageText = `Hola, he realizado un nuevo pedido. Con el ID: ${orderID}`;
     const phoneNumber = process.env.NEXT_PUBLIC_PHONE_NUMBER;
-    window.open(
-      `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(
-        messageText
-      )}`,
-      "_blank"
-    );
-    window.location.href = "/";
+    window.location.href = `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(
+      messageText
+    )}`;
+    setTimeout(() => {
+      setStatus(2)
+    }, 1000);
   };
 
   useEffect(() => {
@@ -39,8 +40,12 @@ function OrderSender() {
   }, []);
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      Enviando tu pedido, por favor no recargues la página.
+    <div className="w-full h-screen flex justify-center items-center p-4 flex-col">
+      {status === 1 ? (
+        <span>Enviando tu pedido, por favor no recargues la página.</span>
+      ) : (
+        <span>Tu pedido fue enviado con éxito. <strong>Gracias por tu compra.</strong><Link href="/" className="font-bold text-blue-700 underline">Volver al inicio</Link></span>
+      )}
     </div>
   );
 }
