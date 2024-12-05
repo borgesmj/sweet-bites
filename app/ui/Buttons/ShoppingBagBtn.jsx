5bedb7d788ef86176d9c949ef1f36e10e0e1f6ea
borgesmj19 <borgesmj19@gmail.com>
2024-12-05 19:18:00 +0000

    hiding shopping bag btn on new order page

--- app/ui/Buttons/ShoppingBagBtn.jsx
+++ app/ui/Buttons/ShoppingBagBtn.jsx
@@ -3,17 +3,21 @@ import { LuShoppingBag } from "react-icons/lu";
 import { openShoppingCartList } from "@/lib/uiHandlers";
 /**
  * * Componente del boton que abre y cierra la ventana de el carrito de compras
- * * Se maneja el numero contenido dentro del span desde la función cartButton en 
+ * * Se maneja el numero contenido dentro del span desde la función cartButton en
  * @link uiHandlers
  * @returns UI del boton
  */
-export default function ShoppingBagBtn({pathName}) {
+export default function ShoppingBagBtn({ pathName }) {
   const handleClick = () => {
     openShoppingCartList();
   };
   return (
     <button
-      className={`relative h-[50px] w-[50px] flex flex-col justify-center items-center   cursor-pointer transition-fast hover:text-[--button-bg-primary] ${["/nosotros", "/galeria", "/contacto"].includes(pathName) ? "opacity-0 cursor-auto" : ""}`}
+      className={`relative h-[50px] w-[50px] flex flex-col justify-center items-center   cursor-pointer transition-fast hover:text-[--button-bg-primary] ${
+        ["/nosotros", "/galeria", "/contacto", "/neworder"].includes(pathName)
+          ? "opacity-0 cursor-auto"
+          : ""
+      }`}
       onClick={handleClick}
       disabled={["/nosotros", "/galeria", "/contacto"].includes(pathName)}
     >
